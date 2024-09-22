013a689a9aa2c50f35340fe5bdd3f8a97daff8a3
Goran Siska <goran.siska@gmail.com>
2024-09-22 14:22:03 +0200

    add invaders

--- Core/Enemies/Invader.gd
+++ Core/Enemies/Invader.gd
@@ -1,10 +1,16 @@
+@tool
 extends Node2D
+
+@export var sprite_frames: SpriteFrames:
+	set(val):
+		sprite_frames = val
 
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animated_sprite_2d.play()
+	self.animated_sprite_2d.sprite_frames = self.sprite_frames
+	self.animated_sprite_2d.play()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
